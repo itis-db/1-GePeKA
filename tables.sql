425d273bcecb4b91105cc1c3c473b8868153dc20
Maxim <111576269+GePeKA@users.noreply.github.com>
2023-10-23 21:08:31 +0300

    Добавил создание таблиц

--- tables.sql
+++ tables.sql
@@ -1,11 +1,46 @@
-
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+create table activitytype (
+	activitytypeid integer primary key,
+	name varchar not null,
+	sysname varchar not null
 );
 
--- 
+create table activity (
+	activityid integer primary key,
+	activitytypeid integer references activitytype (activitytypeid) not null,
+	code varchar not null,
+	name varchar not null,
+	parentid integer references activity (activityid)
+);
+
+create table area (
+	areaid integer primary key,
+	name varchar not null
+);
+
+create table contract (
+	contractid integer primary key references activity (activityid),
+	areaid integer references area (areaid)
+);
+
+create table point (
+	pointid integer primary key references activity (activityid),
+	plandate date not null,
+	factdate date
+);
+
+create table program (
+	programid integer primary key references activity (activityid),
+	indexnum integer,
+	yearstart integer,
+	yearfinish integer
+);
+
+create table project (
+	projectid integer primary key references activity (activityid),
+	targetdescr varchar
+);
+
+create table subprogram (
+	subprogramid integer primary key references activity (activityid),
+	indexnum integer
+);
