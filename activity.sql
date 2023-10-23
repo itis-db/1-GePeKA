264b6a04f800454ed54ad34f5671cbbd6ca3bac4
Maxim <111576269+GePeKA@users.noreply.github.com>
2023-10-24 00:26:05 +0300

    Первая версия

--- activity.sql
+++ activity.sql
@@ -1,1 +1,19 @@
----
+create function activity_levels(pn_activityid int)
+returns table(activityid int, name varchar)
+as $$
+	with recursive r as (
+		select activityid, activitytypeid, name, parentid from activity
+		where activityid = pn_activityid
+
+	  union all
+
+		select a.activityid, a.activitytypeid, a.name, a.parentid from activity as a
+		join r on 
+			a.parentid = r.activityid
+	)
+	
+	select activityid, name from r;
+$$
+language sql;
+
+select * from activity_levels(1);
